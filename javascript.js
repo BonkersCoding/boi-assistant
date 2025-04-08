5ce84313bcbc8ed79281ef60b0cf7a479775ffc0
BonkersCoding <195286253+BonkersCoding@users.noreply.github.com>
2025-04-08 12:35:47 +0200

    Add grid generator and styles

---
+++ javascript.js
@@ -0,0 +1,25 @@
+const container = document.querySelector(".container");
+
+
+function generatePixels(size) {
+    for (let i = 0; i < size; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 0; j < size; j++) {    
+            const pixel = document.createElement("div");
+            pixel.id = "pixel";
+            row.appendChild(pixel);
+        }
+        container.appendChild(row); 
+    }
+}
+
+
+function changeGrid(size) {
+    let rows = document.querySelectorAll(".row");
+    rows.forEach((row) => {container.removeChild(row)});
+    generatePixels(size);
+    pixelNumber = size;
+}
+
+generatePixels(13);
